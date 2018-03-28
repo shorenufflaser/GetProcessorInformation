e16d07076102c953df0f0181f957769c451e2b7a
Neil Shore <neilshore@msn.com>
2018-03-28 18:56:28 +0300

    Added About Box, Added UpdateRate .resx file

--- GetProcessorInformation/Properties/AssemblyInfo.cs
+++ GetProcessorInformation/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GetProcessorInformation")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Report CPU Usage for each processor (real and virtual.  Report overal disk I/O.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tova Technologies")]
 [assembly: AssemblyProduct("GetProcessorInformation")]
-[assembly: AssemblyCopyright("Copyright ©  2017, Neil Shore")]
+[assembly: AssemblyCopyright("Copyright ©  2017, 2018, Neil Shore")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
